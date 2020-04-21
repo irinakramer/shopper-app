197e6588a4fea6e590e0bfdec5cc8f5bf92426a8
Irina Kramer <irakramer@gmail.com>
2020-04-20 19:31:57 -0700

    Updates

---
+++ precache-manifest.4561f8b846a7341f8ea3d90a0259ea78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f10268f36a9011bbff55df4fa2c1295",
+    "url": "/shopper-app/index.html"
+  },
+  {
+    "revision": "a27c62b88a4f6b2cecec",
+    "url": "/shopper-app/static/css/main.59673ef7.chunk.css"
+  },
+  {
+    "revision": "4dd0371b5a3aededd983",
+    "url": "/shopper-app/static/js/2.4d2b270d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopper-app/static/js/2.4d2b270d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a27c62b88a4f6b2cecec",
+    "url": "/shopper-app/static/js/main.2358b5be.chunk.js"
+  },
+  {
+    "revision": "b935e8be727b319dbfe2",
+    "url": "/shopper-app/static/js/runtime-main.839d14ea.js"
+  }
+]);
